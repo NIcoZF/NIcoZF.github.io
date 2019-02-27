49e205301876e8fbae88d15fc3dcec53183e7705
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-27 18:18:11 +0100

    avancee

---
+++ db/migrate/20190227160836_create_places.rb
@@ -0,0 +1,10 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.text :description
+      t.string :opening_hours
+      t.text :comment
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!(name: "La Felicita", description: "Le rendez-vous de la Startup Nation", opening_hours: "9h - 21h", comment: "Très bruyant à partir de 18h");
+
+Place.create!(name: "Bibliothèque Nationale de France", description: "Le rendez-vous de chercheurs", opening_hours: "9h - 21h", comment: "Attention à éviter l'affluence et la file d'attente de 10h");
